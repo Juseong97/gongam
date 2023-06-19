2c2cfbf3c9a46a1fabd205f9b632db476e27fe54
Juseong97 <Juseong97@github.com>
2023-06-19 16:50:45 +0900

    merge

--- c.java
+++ c.java
@@ -10,30 +10,21 @@ public class c extends b{
         System.out.println("git 공부중입니다. by 김주성");
     }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
     // git
-=======
+
     // kjs2 feature에서 수정
->>>>>>> 1441fab90b9be81d962544cb2e6da491cfd3b591
-=======
+
     // kjs2 feature에서 수정
->>>>>>> feature/kjs2
+
     public void kjs(){
         System.out.println("git git git by 김주성");
     }
     public void kjs2(){
         System.out.println("feature kjs-2 modified by 김주성2");
     }
-<<<<<<< HEAD
-<<<<<<< HEAD
+
     public void featureKjs(){
         System.out.println("feature kjs method 입니다");
     }
-=======
 
->>>>>>> 1441fab90b9be81d962544cb2e6da491cfd3b591
-=======
-
->>>>>>> feature/kjs2
 }
