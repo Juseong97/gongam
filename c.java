ff32244aba3d1cec0ac0e09ea1b272b31d48141d
Juseong97 <Juseong97@github.com>
2023-06-19 17:33:30 +0900

    merge

--- c.java
+++ c.java
@@ -23,7 +23,9 @@ public class c extends b{
         System.out.println("feature kjs-2 modified by 김주성2");
     }
 
-
+//    public void mergeTest2(){
+//        System.out.println("kjs가 한것");
+//    }
     public void featureKjs(){
         System.out.println("feature kjs method 입니다");
     }
